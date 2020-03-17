c2b262ca1fe2719ed304525a7b4c47c6bea92b22
TheRaptors <Mcsys@hotmail.com>
2020-03-17 11:09:42 +0800

    新增指数消息

---
+++ stock/stock_index.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import certifi
+import datetime
+import json
+import os
+import re
+import requests
+import time
+import urllib3
+
+# 股票列表
+stocks_list = ['sh000001', 'sz399001', 'sz399006']
+
+stock_api_url = "http://hq.sinajs.cn/list=s_{0}"
+
+robot = 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=a67c801f-75f0-47b8-bf2a-bf032add14f0'
+
+def send_message(msg, robot = robot):
+    headers = {'Content-Type': 'application/json'}
+    data = json.dumps({"msgtype": "markdown", "markdown": {"content": msg}})
+    http = urllib3.PoolManager(cert_reqs = 'CERT_REQUIRED', ca_certs = certifi.where())
+    send = http.request(method = 'POST', url = robot, body = data, headers = headers)
+    print(send.data.decode())
+
+def get_stock_info(stock_id):
+    stock_save = stock_id + '.txt'
+    response = requests.get(url = stock_api_url.format(stock_id))
+    response_text = response.text
+    stock_info = re.search(r"=\"([ \S]*)\"", response_text).group(1).split(",")
+    stock_name = stock_info[0]
+    stock_price = stock_info[1]
+    stock_changePercent = stock_info[2]
+    stock_change = stock_info[3]
+    message = u'【%s】【<font color="#FF0000">%s</font>】，涨幅：【<font color="#FF0000">%s%%</font>】，涨跌：【<font color="#FF0000">%s</font>】' % (stock_name, stock_price, stock_change, stock_changePercent)
+    return message
+
+TIME = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+msg = []
+
+for stock_id in stocks_list:
+    stock_id = stock_id.replace('.', '')
+    message = get_stock_info(stock_id)
+    msg.append(message)
+
+ALL = TIME + '\n' + msg[0] + '\n' + msg[1] + '\n' + msg[2]
+
+send_message(msg = ALL)
